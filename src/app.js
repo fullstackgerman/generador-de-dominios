ab495cec197e7dcdd10cbdcd6b1a2b59adc0d913
German Garcia <fullstackgerman@gmail.com>
2025-10-22 17:10:29 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import "bootstrap";
 import "./style.css";
-
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i of pronoun) {          // Primer bucle: recorre cada pronombre
+  for (let a of adj) {            // Segundo bucle: recorre cada adjetivo
+    for (let n of noun) {         // Tercer bucle: recorre cada sustantivo
+      console.log(i + a + n + ".com");  // Combina y muestra el dominio
+    }
+  }
+}
+}
